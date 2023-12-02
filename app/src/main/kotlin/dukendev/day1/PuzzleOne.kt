bb2fafde5e5ee433bcc5b71d0a3bac23e0bc4da3
dukendev <ysanjeet535@gmail.com>
2023-12-02 13:04:33 +0530

    words matching for most cases

--- app/src/main/kotlin/dukendev/day1/PuzzleOne.kt
+++ app/src/main/kotlin/dukendev/day1/PuzzleOne.kt
@@ -4,9 +4,18 @@ import java.io.File
 
 class PuzzleOne {
 
+
+    private val spelledDigits =
+        setOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+    private val reveredDigits = spelledDigits.map {
+        it.reversed()
+    }
+    private val startMatch = setOf('o', 't', 'f', 'e', 'n')
+    private val endMatch = setOf('e', 'o', 'r', 'x', 'n', 't')
+
     private fun getInputData(): List<String> {
         val file =
-            File("/Users/sanjeetyadav/dev/advent-of-code-2023/app/src/main/kotlin/dukendev/day1/part1.txt")
+            File("/Users/sanjeetyadav/dev/advent-of-code-2023/app/src/main/kotlin/dukendev/day1/input.txt")
         return file.readLines()
     }
 
@@ -16,12 +25,119 @@ class PuzzleOne {
      * @answer : sum of all calibration values
      **/
 
-    fun getSumOfCalibrationValues(input : List<String> = getInputData()): Int {
+    fun getSumOfCalibrationValues(input: List<String> = getInputData()): Int {
         return input.sumOf {
-            val digits = parseNumerals(it)
-            getCalibrationValue(digits.first, digits.second)
+            val digits = parseSpelledNumerals(it)
+            val x = getCalibrationValue(digits.first, digits.second)
+            println(x)
+            x
         }
     }
+
+    fun parseSpelledNumerals(s: String): Pair<Int, Int> {
+        println("---")
+        println("matching : $s")
+        val numeralDigits: Pair<Pair<Int, Int>, Pair<Int, Int>> = getNumeralDigits(s)
+        println("numeral : ${numeralDigits.first} and ${numeralDigits.second}")
+        val spelledDigits: Pair<Pair<Int, Int>, Pair<Int, Int>> = getSpelledDigits(s)
+        println("spell : ${spelledDigits.first} and ${spelledDigits.second}")
+        val first: Int = if (numeralDigits.first.first < spelledDigits.first.first) {
+            numeralDigits.first.second
+        } else {
+            spelledDigits.first.second
+        }
+
+        val second: Int = if (numeralDigits.second.first > spelledDigits.second.first) {
+            numeralDigits.second.second
+        } else {
+            spelledDigits.second.second
+        }
+        println("final : ${first} and ${second}")
+        return Pair(first, second)
+    }
+    private fun getSpelledDigits(s: String): Pair<Pair<Int, Int>, Pair<Int, Int>> {
+        var first: Pair<Int, Int> = Pair(Int.MAX_VALUE, 0)
+        var last: Pair<Int, Int> = Pair(Int.MIN_VALUE, 0)
+        var i = 0
+        var j = 2
+        while (i < s.length && j < s.length) {
+            if (startMatch.contains(s[i])) {
+                val guess = s.slice(i..j)
+                println(guess)
+                if (spelledDigits.contains(guess)) {
+                    println(guess)
+                    first = Pair(i, guess.mapSpell())
+                    break
+                } else {
+                    j++
+                }
+            } else {
+                i++
+            }
+            //reset
+            if(j - i > 6){
+                i++
+                j = i + 2
+            }
+        }
+        println()
+        i = 0
+        j = 2
+        val r = s.reversed()
+        while (i < r.length && j < r.length) {
+            if (endMatch.contains(r[i])) {
+                val guess = r.slice(i..j)
+                if (reveredDigits.contains(guess)) {
+                    println(guess)
+                    last = Pair(r.length - j - 1, guess.reversed().mapSpell())
+                    break
+                } else {
+                    j++
+                }
+            } else {
+                i++
+                j++
+            }
+            //reset
+            if(j - i > 6){
+                i++
+                j = i + 2
+            }
+        }
+
+        return Pair(first, last)
+    }
+    private fun getNumeralDigits(s: String): Pair<Pair<Int, Int>, Pair<Int, Int>> {
+
+        if(s.all { !it.isDigit() }){
+            return Pair(Pair(Int.MAX_VALUE,0), Pair(Int.MIN_VALUE,0))
+        }
+
+        var i = 0
+        var j = s.length - 1
+        while (i < j) {
+            when {
+                s[i].isDigit() && !s[j].isDigit() -> {
+                    j--
+                }
+
+                s[j].isDigit() && !s[i].isDigit() -> {
+                    i++
+                }
+
+                s[i].isDigit() && s[j].isDigit() -> {
+                    return Pair(Pair(i, s[i].digitToInt()), Pair(j, s[j].digitToInt()))
+                }
+
+                else -> {
+                    i++
+                    j--
+                }
+            }
+        }
+        return Pair(Pair(i, s[i].digitToInt()), Pair(j, s[j].digitToInt()))
+    }
+
 
     fun parseNumerals(s: String): Pair<Int, Int> {
         var i = 0
@@ -31,12 +147,15 @@ class PuzzleOne {
                 s[i].isDigit() && !s[j].isDigit() -> {
                     j--
                 }
+
                 s[j].isDigit() && !s[i].isDigit() -> {
                     i++
                 }
+
                 s[i].isDigit() && s[j].isDigit() -> {
                     return Pair(s[i].digitToInt(), s[j].digitToInt())
                 }
+
                 else -> {
                     i++
                     j--
@@ -44,6 +163,21 @@ class PuzzleOne {
             }
         }
         return Pair(s[i].digitToInt(), s[j].digitToInt())
+    }
+
+    private fun String.mapSpell(): Int {
+        return when (this) {
+            "one" -> 1
+            "two" -> 2
+            "three" -> 3
+            "four" -> 4
+            "five" -> 5
+            "six" -> 6
+            "seven" -> 7
+            "eight" -> 8
+            "nine" -> 9
+            else -> 0
+        }
     }
     private fun getCalibrationValue(first: Int, second: Int): Int = 10.times(first).plus(second)
 
--- app/src/test/kotlin/dukendev/TestPuzzle1.kt
+++ app/src/test/kotlin/dukendev/TestPuzzle1.kt
@@ -10,6 +10,9 @@ import kotlin.test.assertEquals
 class TestPuzzle1 {
 
 
+
+
+
     @Test
     fun parseNumeralsTest() {
         val puzzleOne = PuzzleOne()
@@ -22,11 +25,29 @@ class TestPuzzle1 {
         val testInput = listOf(
             "1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"
         )
+        val testInput2 = listOf(
+            "two1nine",
+            "eightwothree",
+            "abcone2threexyz",
+            "xtwone3four",
+            "4nineeightseven2",
+            "zoneight234",
+            "7pqrstsixteen",
+            "2fourghsixptk"
+        )
         val puzzleOne = PuzzleOne()
         assertEquals(142, puzzleOne.getSumOfCalibrationValues(testInput))
+        assertEquals(307, puzzleOne.getSumOfCalibrationValues(testInput2))
     }
 
 
+    @Test fun parseSpellTest() {
+        val puzzleOne = PuzzleOne()
+        assertEquals(Pair(3, 4), puzzleOne.parseSpelledNumerals("cpxtthree14"))
+        assertEquals(Pair(8,3),puzzleOne.parseSpelledNumerals("eightwothree"))
+        assertEquals(Pair(3,2),puzzleOne.parseSpelledNumerals("3oneeighttwo"))
+        assertEquals(Pair(2,6),puzzleOne.parseSpelledNumerals("2fourghsixptk"))
+    }
 
 
 }
