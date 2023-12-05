0c609b3a8e9fe7b2fda69bf7bdd5c9c207364336
dukendev <ysanjeet535@gmail.com>
2023-12-05 12:37:59 +0530

    solved example

--- app/src/main/kotlin/dukendev/day3/PuzzleThree.kt
+++ app/src/main/kotlin/dukendev/day3/PuzzleThree.kt
@@ -7,7 +7,8 @@ class PuzzleThree {
     private val path =
         "/Users/sanjeetyadav/dev/adventOfCode2023/advent-of-code-2023-kotlin/app/src/main/kotlin/dukendev/day3/input.txt"
 
-    private val testPath = "/Users/sanjeetyadav/dev/adventOfCode2023/advent-of-code-2023-kotlin/app/src/main/kotlin/dukendev/day3/test.txt"
+    private val testPath =
+        "/Users/sanjeetyadav/dev/adventOfCode2023/advent-of-code-2023-kotlin/app/src/main/kotlin/dukendev/day3/test.txt"
 
     fun findSumOfEngineParts() {
         val lines = getInputData(testPath)
@@ -19,32 +20,40 @@ class PuzzleThree {
         val rows = grid.size - 1
         val num = mutableListOf<Char>()
         for (i in 0..rows) {
+            var matchStart = cols
             for (j in 0..cols) {
                 val c = grid[i][j]
                 if (c.isDigit()) {
-                    if (notValidNumber(
-                            r = i,
-                            c = j,
-                            grid = grid,
-                            isFirst = num.isEmpty(),
-                            isLast = !(grid[i].getOrElse(j + 1) { '.' }.isDigit())
-                        )
-                    ) {
-                        println("not valid number ${num.toNumber()} at ($i,$j)")
-                        num.clear()
-                    } else {
-                        num.add(grid[i][j])
-                    }
+                    num.add(grid[i][j])
+                    matchStart = minOf(matchStart, j)
+                    println("number building : $num and start match $matchStart")
                 } else {
                     //check to push in main list or clear
                     if (num.isNotEmpty()) {
-                        println("found ${num.toNumber()} at ($i,$j)")
-                        valid.add(num.toNumber())
+                        println("validating $num from $matchStart to ${j - 1}")
+                        var isValid = true
+                        for (digit in matchStart until j) {
+                            isValid = isValid && alone(
+                                r = i,
+                                c = digit,
+                                grid = grid,
+                                isFirst = digit == matchStart,
+                                isLast = digit == j - 1
+                            )
+
+                        }
+                        if (!isValid) {
+                            println("found ${num.toNumber()} at ($i,${j - 1})")
+                            valid.add(num.toNumber())
+                        }
                         num.clear()
+                        matchStart = cols
                     }
+
                 }
             }
         }
+        println(valid)
         println(valid.sum())
     }
 
@@ -55,28 +64,60 @@ class PuzzleThree {
     }
 
 
-    private fun notValidNumber(
+    private fun alone(
         r: Int, c: Int, grid: List<List<Char>>, isLast: Boolean = false, isFirst: Boolean = false
     ): Boolean {
+        print("checking ${grid[r][c]} at ($r,$c)")
+        val up = (if (r > 0) grid.getOrNull(r - 1)?.get(c)  else '.') == '.'
+        val down = (if (r < grid.size - 1) grid.getOrNull(r + 1)?.get(c) else '.') == '.'
+        val back = grid[r].getOrElse(c - 1) { '.' } == '.'
+        val diaBackDown =
+            (if (r < grid.size - 1) grid.getOrNull(r + 1)?.getOrElse(c - 1) { '.' } else '.') == '.'
+        val diaBackUp =
+            (if (r > 0) grid.getOrNull(r - 1)?.getOrElse(c - 1) { '.' } else '.') == '.'
+        val diaFrontUp =
+            (if (r > 0) grid.getOrNull(r - 1)?.getOrElse(c + 1) { '.' } else '.') == '.'
+        val diaFrontDown =
+            (if (r < grid.size - 1) grid.getOrNull(r + 1)?.getOrElse(c + 1) { '.' } else '.') == '.'
+        val front = grid[r].getOrElse(c + 1) { '.' } == '.'
+
+        val representation = listOf(
+            listOf(diaBackUp.printC(), up.printC(), diaFrontUp.printC()),
+            listOf(back.printC(), 'd', front.printC()),
+            listOf(diaBackDown.printC(), down.printC(), diaFrontDown.printC())
+        )
+
+        println()
+        for (rw in representation) {
+            for (cl in rw) {
+                print(" $cl ")
+            }
+            println()
+        }
+
         return when {
             isFirst && !isLast -> {
-                grid.getOrNull(r - 1)?.get(c) == '.' && grid.getOrNull(r + 1)
-                    ?.get(c) == '.' && grid[r].getOrElse(c - 1) { '.' } == '.' && grid.getOrNull(r + 1)
-                    ?.getOrElse(c - 1) { '.' } == '.' && grid.getOrNull(r - 1)
-                    ?.getOrElse(c - 1) { '.' } == '.'
+                val first = down && up && back && diaBackUp && diaBackDown && diaFrontDown && diaFrontUp
+                println("first alone $first")
+                first
             }
 
             isLast && !isFirst -> {
-                grid.getOrNull(r - 1)?.get(c) == '.' && grid.getOrNull(r + 1)
-                    ?.get(c) == '.' && grid[r].getOrElse(c + 1) { '.' } == '.' && grid.getOrNull(r + 1)
-                    ?.getOrElse(c + 1) { '.' } == '.' && grid.getOrNull(r - 1)
-                    ?.getOrElse(c + 1) { '.' } == '.'
+                val last = down && up && front && diaFrontUp && diaFrontDown && diaBackUp && diaBackDown
+                println("last alone : $last")
+                last
             }
 
             else -> {
-                grid.getOrNull(r - 1)?.get(c) == '.' && grid.getOrNull(r + 1)?.get(c) == '.'
+                val mid = down && up && diaBackUp && diaBackDown && diaFrontDown && diaFrontUp
+                println("mid alone : $mid")
+                mid
             }
         }
+    }
+
+    private fun Boolean.printC(): Char {
+        return if (this) '.' else '+'
     }
 
 }
