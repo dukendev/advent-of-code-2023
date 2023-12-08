66341e8e23467a632a2658287c17c4231b350ef7
dukendev <ysanjeet535@gmail.com>
2023-12-09 03:50:07 +0530

    started improving typeorder sorting

--- app/src/main/kotlin/dukendev/day7/PuzzleSeven.kt
+++ app/src/main/kotlin/dukendev/day7/PuzzleSeven.kt
@@ -5,18 +5,22 @@ import dukendev.util.Util
 class PuzzleSeven {
 
     fun calcWinnings() {
-        val input = Util.getInputData(test)
+        val input = Util.getInputData(input)
         val cards = input.map {
             CamelCard(
                 bid = it.split(" ")[1].trim().toInt(),
                 hand = it.split(" ")[0].trim().toCharArray().toList(),
-                typeOrder = 0
+                typeOrder = 0,
+                freqMap = mapOf()
             )
         }.map {
             it.copy(typeOrder = it.hand.findOrder())
         }.map { card ->
             card.copy(hand = card.hand.sortedBy { it.getCardValue() })
+        }.map {
+            it.copy(freqMap = it.hand.calculateFrequency())
         }
+
         val orderedLists = cards.sortedBy { it.typeOrder }.groupBy { it.typeOrder }.map { it.value }
             .map { list -> list.sortByHighCards() }
         val sortedCards = orderedLists.flatten()
@@ -29,9 +33,39 @@ class PuzzleSeven {
     }
 
     private fun List<CamelCard>.sortByHighCards(): List<CamelCard> {
-        return this.asSequence().sortedBy { it.hand[0].getCardValue() }
-            .sortedBy { it.hand[1].getCardValue() }.sortedBy { it.hand[2].getCardValue() }
-            .sortedBy { it.hand[3].getCardValue() }.sortedBy { it.hand[4].getCardValue() }.toList()
+        val sortedList = mutableListOf<CamelCard>()
+        when (this.first().typeOrder) {
+            7 -> {
+                sortedList.addAll(this.sortedBy { it.hand.first().getCardValue() })
+            }
+
+            6, 5 -> {
+                sortedList.addAll(this.sortedBy {
+                    if (it.freqMap[it.hand.first()]!! < it.freqMap[it.hand.last()]!!) {
+                        it.hand.first().getCardValue()
+                    } else {
+                        it.hand.last().getCardValue()
+                    }
+                }.sortedBy {
+                    if (it.freqMap[it.hand.first()]!! > it.freqMap[it.hand.last()]!!) {
+                        it.hand.first().getCardValue()
+                    } else {
+                        it.hand.last().getCardValue()
+                    }
+                })
+            }
+            // todo : impl for 4 to 2 case for type order
+            else -> {
+                sortedList.addAll(
+                    this.asSequence().sortedBy { it.hand[0].getCardValue() }
+                        .sortedBy { it.hand[1].getCardValue() }
+                        .sortedBy { it.hand[2].getCardValue() }
+                        .sortedBy { it.hand[3].getCardValue() }
+                        .sortedBy { it.hand[4].getCardValue() }.toList()
+                )
+            }
+        }
+        return sortedList
     }
 
     private fun Char.getCardValue(): Int {
@@ -45,7 +79,7 @@ class PuzzleSeven {
         }
     }
 
-    private fun List<Char>.findOrder(): Int {
+    private fun List<Char>.calculateFrequency(): MutableMap<Char, Int> {
         val frequency = mutableMapOf<Char, Int>()
         for (c in this) {
             if (frequency.contains(c)) {
@@ -54,13 +88,18 @@ class PuzzleSeven {
                 frequency.putIfAbsent(c, 1)
             }
         }
+        return frequency
+    }
+
+    private fun List<Char>.findOrder(): Int {
+        val frequency = this.calculateFrequency()
         //find order from frequency map
         return when (frequency.keys.size) {
-            1 -> 7
-            2 -> if (frequency.keys.any { frequency[it] == 4 }) 6 else 5
-            3 -> if (frequency.keys.any { frequency[it] == 3 }) 4 else 3
-            4 -> 2
-            else -> 1
+            1 -> 7 //five of a kind
+            2 -> if (frequency.keys.any { frequency[it] == 4 }) 6 else 5 //four of a kind or full house
+            3 -> if (frequency.keys.any { frequency[it] == 3 }) 4 else 3 //three of a kind or 2 pair
+            4 -> 2 // 1 pair
+            else -> 1 // high card
         }
     }
 
@@ -73,6 +112,5 @@ class PuzzleSeven {
 }
 
 data class CamelCard(
-    val hand: List<Char>, val bid: Int, val typeOrder: Int
-
+    val hand: List<Char>, val bid: Int, val typeOrder: Int, val freqMap: Map<Char, Int>
 )
